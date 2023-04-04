9992d471febc1c1542ac202fe43d93daf1e2950c
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-04 16:51:29 -0700

    feat: update tuits schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,9 +1,20 @@
 import mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({
+  topic: String,
+  userName: String,
+  handle: String,
+  time: String,
+  image: String,
+  title: String,
   tuit: String,
-  likes: Number,
   liked: Boolean,
+  likes: Number,
+  replies: Number,
+  retuits: Number,
+  retuited: Boolean,
+  dislikes: Number,
+  disliked: Boolean,
 }, {collection: 'tuits'});
 export default schema;
 
